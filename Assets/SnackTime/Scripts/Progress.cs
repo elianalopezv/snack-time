c04417b274a5b1a8be1b91fa624f7aabbfbfb641
Eliana <eliana.lopez@yuxiglobal.com>
2018-09-18 16:38:26 -0500

    initial progresss bar

---
+++ Assets/SnackTime/Scripts/Progress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Progress : MonoBehaviour {
+
+	public RectTransform playerIcon;
+	public RectTransform snackIcon; 
+	private float totalGameTime, speed;
+	private Vector2 initialPlayerIconPos;
+
+	// Use this for initialization
+	void Start () {
+		initialPlayerIconPos = playerIcon.localPosition;
+		totalGameTime = LevelManager.Instance.totalGameTime;
+		speed = Mathf.Abs(snackIcon.localPosition.x - playerIcon.localPosition.x) / totalGameTime;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
