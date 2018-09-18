baf9f880456bb27ae2f02ff142bfe6ac974008c7
Eliana <eliana.lopez@yuxiglobal.com>
2018-09-18 15:25:54 -0500

    new jump style

--- Assets/SnackTime/Scripts/Player.cs
+++ Assets/SnackTime/Scripts/Player.cs
@@ -4,26 +4,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public float jumpSpeed;
-	public float gravity;
+	public float speed = 9f;
+ 	public float jumpSpeed = 20f;
+ 	public float gravity = 20f;
+ 	public float gravityForce = 3f; 
+ 	public float airTime = 2f;
 	private CharacterController controller;
 	private Vector3 moveDirection = Vector3.zero;
+	private float jumpForce = 0;
+ 	private float invertGravity;
+	private bool mouseUp = true;
+
 
 	// Use this for initialization
 	void Start () {
-		controller = GetComponent<CharacterController>();
+		controller = GetComponent<CharacterController>();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if(Input.GetMouseButton(0) && controller.isGrounded)
-		{
-			moveDirection.y = jumpSpeed;
+
+		moveDirection *= speed;    
+		if (controller.isGrounded && mouseUp) {
+			// jumpForce is 0 if player grounded
+			jumpForce = 0;
+			// invertGravity is also reset based on the gravity
+			invertGravity = gravity * airTime;
+
+			if (Input.GetMouseButton(0)){
+				// Jump! 
+				jumpForce = jumpSpeed;
+				mouseUp = false;
+			}
 		}
 
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
+		// Jumping since jumpForce is not 0
+		// we add invertGravity to our jumpForce and invertGravity is also
+		// decreased so that we get a curvy jump
+		if(Input.GetMouseButton(0) && jumpForce != 0){
+			invertGravity -= Time.deltaTime;
+			jumpForce += invertGravity * Time.deltaTime;
+		} 
+		// Here we apply the gravity
+			jumpForce -= gravity*Time.deltaTime* gravityForce;
+			moveDirection.y = jumpForce;
+			controller.Move(moveDirection * Time.deltaTime);
+		
+		//Avoid holding click infinitely
+		if(Input.GetMouseButtonUp(0))
+		{
+			mouseUp = true;
+		}
+		
 	}
 
 	void OnBecameInvisible()
